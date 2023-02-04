885a556f5a176b378ef7636e769e5184947f4c14
byuimacann <mac20051@byui.edu>
2023-02-04 14:03:17 -0700

    added javascript banner and social icons

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -10,10 +10,10 @@ x.onclick = toggleMenu;
 
 
 //Last Modified Date in Footer//
+const lastModDate = new Date();
 const modDate = document.lastModified;
-const lastModDate = new Date(modDate);
 
-document.getElementById("lastMod").innerHTML = `Last Updated: ${new Date(modDate)}`;
+document.getElementById("lastMod").innerHTML = `Last Updated: ${modDate}`;
 
 
 
@@ -21,9 +21,29 @@ document.getElementById("lastMod").innerHTML = `Last Updated: ${new Date(modDate
 const datearea = document.querySelector("#bottomofheader");
 
 const today = new Date();
-const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
+const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
 
-datearea.innerHTML = fulldate;
+datearea.innerHTML = fullDate;
+
+
+
+//Monday or Tuesday banner for home page
+const banner = document.querySelector('#banner');
+
+const bannerDate = today.getDay();
+let message = "";
+
+  if (bannerDate == (1 || 2)){ 
+    message = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    banner.style.display = "block";
+  } else {
+    banner.style.display = "none";
+  }
+  
+  banner.innerHTML = message;
+
+
+
 
 
 
