f533d984c2c2b0b13e471714020025a3855eaa33
byuimacann <mac20051@byui.edu>
2023-02-16 22:16:35 -0700

    fixed javascript windchill function

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -23,10 +23,10 @@ let temp = parseInt(span1.value);
 let wS = parseInt(span2.value);
 
 if ((temp <= 50) && (wS > 3)) {
-let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wS .16) + 0.4275 * temp * Math.pow(wS, .16);
+let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wS, .16) + 0.4275 * temp * Math.pow(wS, .16);
 }
 else {
    f = "N/A";
 } 
 
-span3.innerHTML = f.toFixed(1);
+span3.innerHTML = f;
