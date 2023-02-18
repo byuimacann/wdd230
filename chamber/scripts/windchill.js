67c567336f4549348161413922d45e82fe00658d
byuimacann <mac20051@byui.edu>
2023-02-17 17:20:22 -0700

    trying to fix windchill javascript

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,32 +1,19 @@
-/*const span3 = document.getElementById("wChill");
+const span1 = document.querySelector("#tempF");
+const span2 = document.querySelector("#wSpeed");
+const span3 = document.querySelector("#wChill");
 
-let temp = parseInt(document.getElementById("tempF").value);
-let wS = parseFloat(document.getElementById("wSpeed").value);
-let wC = windChill(temp, wS);
-span3.innerHTML = wC.toFixed(1);
-    
-function windChill (tF, speed){
-if ((tF <= 50) && (speed > 3)) {
-  wC = 35.74 + 0.6215 * tF - 35.75 * Math.pow(speed .16) + 0.4275 * tF * Math.pow(speed, .16); 
+let temp = parseFloat(span1.value);
+let speed = parseFloat(span2.value);
+let wC = windChill(temp, speed);
+span3.innerHTML = wC;
+
+function windChill(tF, wS){
+if (tF <= 50 && wS > 3) {
+  let result = 35.74 + 0.6215 * tF - 35.75 * Math.pow(wS, .16) + 0.4275 * tF * Math.pow(wS, .16);
+  return result; 
 }
-else {
-  wC = "N/A"
-  return wC;
+  else {
+  result = "N/A"
+  return result;
 }
-}*/
-
-const span1 = document.getElementById("tempF");
-const span2 = document.getElementById("wSpeed");
-const span3 = document.getElementById("wChill");
-
-let temp = parseInt(span1.value);
-let wS = parseInt(span2.value);
-
-if ((temp <= 50) && (wS > 3)) {
-let f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wS, .16) + 0.4275 * temp * Math.pow(wS, .16);
 }
-else {
-   f = "N/A";
-} 
-
-span3.innerHTML = f;
