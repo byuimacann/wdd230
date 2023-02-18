1b81e1168a93eadd26a35e1e73f087c4badd6641
byuimacann <mac20051@byui.edu>
2023-02-18 14:46:03 -0700

    fixed windchill javascript - now working

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -2,18 +2,19 @@ const span1 = document.querySelector("#tempF");
 const span2 = document.querySelector("#wSpeed");
 const span3 = document.querySelector("#wChill");
 
-let temp = parseFloat(span1.value);
-let speed = parseFloat(span2.value);
+let temp = parseFloat((span1).innerText);
+let speed = parseFloat((span2).innerText);
 let wC = windChill(temp, speed);
-span3.innerHTML = wC;
+span3.innerHTML = wC.toFixed(1);
 
 function windChill(tF, wS){
+let f; 
 if (tF <= 50 && wS > 3) {
-  let result = 35.74 + 0.6215 * tF - 35.75 * Math.pow(wS, .16) + 0.4275 * tF * Math.pow(wS, .16);
-  return result; 
+  f = 35.74 + 0.6215 * tF - 35.75 * Math.pow(wS, .16) + 0.4275 * tF * Math.pow(wS, .16);
+  return f; 
 }
   else {
-  result = "N/A"
-  return result;
+  f = "N/A"
+  return f;
 }
 }
