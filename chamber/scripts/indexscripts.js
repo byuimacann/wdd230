f0cf4c1938447d2b36df6ac14034712a39f3e1ce
byuimacann <mac20051@byui.edu>
2023-02-25 23:57:00 -0700

    form and thank you html pages

--- chamber/scripts/indexscripts.js
+++ chamber/scripts/indexscripts.js
@@ -43,6 +43,10 @@ let message = "";
   banner.innerHTML = message;
 
 
+  
+  
+
+
 
 
 
---
+++ chamber/scripts/joinscripts.js
@@ -0,0 +1,5 @@
+//Hidden Date and Time for form
+const dateTime = document.querySelector("#dateTime");
+
+let today = new Date();
+dateTime.value = today;
